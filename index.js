e3dc0e12b0851445f309390a3fcda6e46da97320
damyco <25842807+damyco@users.noreply.github.com>
2021-07-10 09:34:13 +0100

    heroku setup

--- index.js
+++ index.js
@@ -88,6 +88,8 @@ app.get("/api/notes", (request, response) => {
   response.json(notes);
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
