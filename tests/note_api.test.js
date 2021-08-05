876a2c64d72c4fcfdd3f643313b7a49113931c91
damyco <25842807+damyco@users.noreply.github.com>
2021-08-05 17:38:47 +0100

    more tests! async/await refactor

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -53,6 +53,37 @@ test("a specific note is within the returned notes", async () => {
   expect(contents).toContain("Browser can execute only Javascript");
 });
 
+test('a specific note can be viewed', async () => {
+    const notesAtStart = await helper.notesInDb()
+    const noteToView = notesAtStart[0]
+
+    const resultNote = await api
+        .get(`/api/notes/${noteToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    
+    const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
+
+    expect(resultNote.body).toEqual(processedNoteToView)
+})
+
+test('a note can be deleted', async () => {
+    const notesAtStart = await helper.notesInDb()
+    const noteToDelete = notesAtStart[0]
+
+    await api
+        .delete(`/api/notes/${noteToDelete.id}`)
+        .expect(204)
+    
+    const notesAtEnd = await helper.notesInDb()
+    
+    expect(notesAtEnd).toHaveLength(helper.initialNotes.length - 1)
+
+    const contents = notesAtEnd.map(r => r.content)
+
+    expect(contents).not.toContain(noteToDelete.content)
+})
+
 // test that adds a new note and verifies that the amount of notes returned by the API increases, and that the newly added note is in the list.
 test("a valid note can be added", async () => {
   const newNote = {
